e2b67ebf26a71e0351ce9fe0d0b84d7eb8b49250
SEBASTIAN OCAMPO HERNANDEZ <sebastian-ocampoh@unilibre.edu.co>
2018-10-12 10:25:15 -0500

    Parcial Terminado Full

---
+++ Parcial2/Parcial2/Controllers/ClienteController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Parcial2.Models;
+
+namespace Parcial2.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Random()
+        {
+            var Cliente = new Clientes()
+
+            {
+                Nombre = "Sebastian",
+                Apellido = "Ocampo Hernandez",
+                Sueldo = 3500000
+            };
+
+
+            return View(Cliente);
+        }
+    }
+}
---
+++ Parcial2/Parcial2/Models/Clientes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Parcial2.Models
+{
+    public class Clientes
+
+    {
+        public int Id { get; set; }
+        public String Nombre { get; set; }
+        public String Apellido { get; set; }
+        public float Sueldo { get; set; }
+    }
+}
